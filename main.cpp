35e6b7d2afe1cd3f33c9a8a003f868c9c4f71028
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-25 21:39:35 +0100

    C++17 and shorter

--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
-#include <iostream>
-
-//C++14
-auto f() noexcept {
-  return "Hello world\n";
-}
-
 int main() 
 {
+  static_assert("C++17");
   int a[3] = { 0, 1, 2 };
-  a[3] = 0; // Access violation!
-  std::cout << a[0] << f() << '\n';
+  a[3] = 4; //Access violation
 }
